6724d76b38c475ba4b22e1ca009879986406e2d0
Sergey Solozhentsev <ssolozhentsev@runmylease.com>
2024-07-29 07:45:31 +0200

    Fixed import other scss

--- src/utils.ts
+++ src/utils.ts
@@ -57,7 +57,7 @@ export const getScssClasses = async (scssFile: string) => {
   const scssClasses = new Map<string, ScssClass>();
 
   const fileContent = readFileSync(scssFile, "utf-8");
-  const processedCss = compileString(fileContent, { sourceMap: true });
+  const processedCss = compileString(fileContent, { sourceMap: true, url: new URL(`file:///${scssFile}`) });
   const decodedMappings = decodeSourceMap(processedCss.sourceMap!.mappings);
   const positionMap = new Map<string, CodePosition>();
   for (let i = 0; i < decodedMappings.length; i++) {
